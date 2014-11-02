442a4638533550ea6ee50023571631450a6b94d0
Eric Nelson <ericnelson1@users.noreply.github.com>
2014-11-02 00:30:51 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,21 @@
-﻿# powershell profile
+# powershell profile
+# ii $profile
 
-Set-ExecutionPolicy RemoteSigned
+# Set-ExecutionPolicy RemoteSigned
 
 # node and git use black
-(Get-Host).UI.RawUI.BackgroundColor=Black
+$host.UI.RawUI.BackgroundColor="Black"
 
 # set the error background to decency
-$pd = (Get-Host).PrivateData
-$pd.ErrorBackgroundColor = "DarkMagenta" #"darkblue"
+# $host.PrivateData.ErrorBackgroundColor = "Black"
+
+# colored prompt
+function Prompt
+{
+    $promptString = "PS " + $(Get-Location) + ">"
+    Write-Host $promptString -NoNewline -ForegroundColor White
+    return " "
+}
 
 # display path environment variable per line
 function Get-PathPerLine()
@@ -20,14 +28,6 @@ $env:Path = $env:Path + ";C:\Program Files (x86)\Notepad++"
 $env:Path = $env:Path + ";C:\Program Files\Sublime Text 2"
 $env:Path = $env:Path + ";C:\Program Files (x86)\WinMerge"
 $env:Path = $env:Path + ";C:\Program Files (x86)\Git\bin"
-
-# colored prompt
-function Prompt
-{
-    $promptString = "PS " + $(Get-Location) + ">"
-    Write-Host $promptString -NoNewline -ForegroundColor Yellow
-    return " "
-}
 
 Set-Alias path Get-PathPerLine
 Set-Alias sublime sublime_text.exe
