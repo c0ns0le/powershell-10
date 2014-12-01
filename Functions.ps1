1eb77ee6a5280b52b45846ed0eb0e8ff87c16052
Eric Nelson <ericnelson1@users.noreply.github.com>
2014-12-01 08:47:34 -0600

    Update Functions.ps1

--- Functions.ps1
+++ Functions.ps1
@@ -5,41 +5,41 @@
 
 
 param([switch]$csv, [switch]$recurse)
- 
+Â 
 [Reflection.Assembly]::LoadWithPartialName("System.Security") | out-null
 $sha1 = new-Object System.Security.Cryptography.SHA1Managed
 $pathLength = (get-location).Path.Length + 1
 # $StringBuilder = New-Object System.Text.StringBuilder 50
 
 $args | %{
-    if ($recurse) {
-        $files = get-childitem -recurse -include $_
-    }
-    else {
-        $files = get-childitem -include $_
-    }
- 
-    if ($files.Count -gt 0) {
-        $files | %{
+Â Â Â Â if ($recurse) {
+Â Â Â Â Â Â Â Â $files = get-childitem -recurse -include $_
+Â Â Â Â }
+Â Â Â Â else {
+Â Â Â Â Â Â Â Â $files = get-childitem -include $_
+Â Â Â Â }
+Â 
+Â Â Â Â if ($files.Count -gt 0) {
+Â Â Â Â Â Â Â Â $files | %{
 
-            $filename = $_.FullName
-            $filenameDisplay = $filename.Substring($pathLength)
-             
-            if ($csv) {
-                write-host -NoNewLine ($filenameDisplay + ",")
-            } else {
-                write-host $filenameDisplay
-            }
- 
-            $file = [System.IO.File]::Open($filename, "open", "read")
-            $sha1.ComputeHash($file) | %{
-                write-host -NoNewLine $_.ToString("x2")
-            }
-            $file.Dispose()
- 
-            write-host
-            if ($csv -eq $false) {
-                write-host
+Â Â Â Â Â Â Â Â Â Â Â Â $filename = $_.FullName
+Â Â Â Â Â Â Â Â Â Â Â Â $filenameDisplay = $filename.Substring($pathLength)
+Â Â Â Â Â Â Â Â Â Â Â Â Â 
+Â Â Â Â Â Â Â Â Â Â Â Â if ($csv) {
+Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â write-host -NoNewLine ($filenameDisplay + ",")
+Â Â Â Â Â Â Â Â Â Â Â Â } else {
+Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â write-host $filenameDisplay
+Â Â Â Â Â Â Â Â Â Â Â Â }
+Â 
+Â Â Â Â Â Â Â Â Â Â Â Â $file = [System.IO.File]::Open($filename, "open", "read")
+Â Â Â Â Â Â Â Â Â Â Â Â $sha1.ComputeHash($file) | %{
+Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â write-host -NoNewLine $_.ToString("x2")
+Â Â Â Â Â Â Â Â Â Â Â Â }
+Â Â Â Â Â Â Â Â Â Â Â Â $file.Dispose()
+Â 
+Â Â Â Â Â Â Â Â Â Â Â Â write-host
+Â Â Â Â Â Â Â Â Â Â Â Â if ($csv -eq $false) {
+Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â Â write-host
 
 <#
             $file = [System.IO.File]::Open($filename, "open", "read")
@@ -48,16 +48,60 @@ $args | %{
             }
             $file.Dispose()
 
-            $object = New-Object –TypeName PSObject
-            $object | Add-Member –MemberType NoteProperty –Name filename –Value $filenameDisplay
-            $object | Add-Member –MemberType NoteProperty –Name sha –Value $StringBuilder.ToString();
+            $object = New-Object â€“TypeName PSObject
+            $object | Add-Member â€“MemberType NoteProperty â€“Name filename â€“Value $filenameDisplay
+            $object | Add-Member â€“MemberType NoteProperty â€“Name sha â€“Value $StringBuilder.ToString();
             Write-Output $object
             [void]$StringBuilder.Clear();
 
             #>
 
 
-            }
-        }
-    }
+Â Â Â Â Â Â Â Â Â Â Â Â }
+Â Â Â Â Â Â Â Â }
+Â Â Â Â }
 }
+
+
+
+function OutputFormattedBytes ($bytes)
+{
+                Write-Output "Hexadecimal:"
+                [BitConverter]::ToString($bytes)
+
+                Write-Output "Hexadecimal (without dashes):"
+                [BitConverter]::ToString($bytes).Replace('-', '')
+
+                Write-Output "Base64:"
+                [Convert]::ToBase64String($bytes)
+
+                Write-Output "Base64 URL:"
+                [Convert]::ToBase64String($bytes).Replace('+', '-').Replace('/', '_')
+}
+
+function OutputHashes ($data)
+{
+                $data = [byte[]]$data
+                $hashMD5 = New-Object Security.Cryptography.MD5CryptoServiceProvider
+                $hashSHA1 = New-Object Security.Cryptography.SHA1Managed
+                $hashSHA256 = New-Object Security.Cryptography.SHA256Managed
+                $hashSHA512 = New-Object Security.Cryptography.SHA512Managed
+
+                Write-Output "`nMD5"
+                OutputFormattedBytes $hashMD5.ComputeHash($data)
+                
+                Write-Output "`nSHA1"
+                OutputFormattedBytes $hashSHA1.ComputeHash($data)
+                
+                Write-Output "`nSHA256"
+                OutputFormattedBytes $hashSHA256.ComputeHash($data)
+                
+                Write-Output "`nSHA512"
+                OutputFormattedBytes $hashSHA512.ComputeHash($data)
+}
+
+# make the window wide for easier copying of hashes
+[console]::WindowWidth = [console]::LargestWindowWidth - 5
+
+#hash three bytes
+OutputHashes @(255, 1, 3) 
