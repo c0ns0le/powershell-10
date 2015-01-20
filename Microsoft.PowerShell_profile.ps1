b1211a567017cebf41ee302a2d0e67e0ac9a755b
Eric Nelson <ericnelson1@users.noreply.github.com>
2015-01-19 23:26:19 -0600

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,7 +11,7 @@ $host.UI.RawUI.BackgroundColor="Black"
 # set the error background to decency
 # $host.PrivateData.ErrorBackgroundColor = "Black"
 
-# colored prompt
+# color prompt
 function Prompt
 {
     $promptString =  "$pwd`nPS >"
@@ -23,6 +23,40 @@ function Prompt
 function Get-PathPerLine()
 {
     $env:Path.Split(";")
+}
+
+# alias reverse lookup
+function Lookup-Alias($cmd)
+{
+    gcm $cmd | % {gal -Definition $_.name -ea 0}
+}
+
+function show-colors()
+{
+    [enum]::GetValues([System.ConsoleColor]) | Foreach-Object {Write-Host $_ -ForegroundColor $_} 
+}
+
+function kill-all($name)
+{
+    ps|where{$_.name -like "*$name*"}|foreach{$_.kill()}
+}
+
+# replace with type accelerator module
+[System.Type]$typeAcceleratorsType =
+[System.Management.Automation.PSObject].Assembly.GetType(
+'System.Management.Automation.TypeAccelerators',
+$true,
+$true
+)
+# replace with type accelerator module
+function get-typeaccelerators()
+{
+    $typeAcceleratorsType::get
+}
+
+function load-assembly($namespace)
+{
+    [Reflection.Assembly]::LoadWithPartialName($namespace)
 }
 
 $env:NODE_PATH = "C:\Users\Eric\AppData\Roaming\npm\node_modules"
@@ -37,6 +71,13 @@ $env:Path = $env:Path + ";C:\Program Files\nodejs"
 $env:Path = $env:Path + ";C:\Users\Eric\AppData\Roaming\npm"
 $env:Path = $env:Path + ";" + $env:NODE_PATH
 
+
 Set-Alias path Get-PathPerLine
-Set-Alias sub sublime_text.exe
+Set-Alias sublime sublime_text.exe
 Set-Alias npp notepad++.exe
+
+Set-Alias ss select-string
+Set-Alias gh set-help
+Set-Alias no New-Object
+
+clear
