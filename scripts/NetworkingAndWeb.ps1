940d59e4d8b890c5c83a19856a97e81247b268f0
Eric Nelson <ericnelson1@users.noreply.github.com>
2016-03-10 00:04:50 -0600

    Update NetworkingAndWeb.ps1

--- scripts/NetworkingAndWeb.ps1
+++ scripts/NetworkingAndWeb.ps1
@@ -21,5 +21,15 @@ $webservice | get-member
 $webservice.GetInfoByCity('New York').Table 
 $webservice.GetInfoByZIP('10286').Table
 
+$uri = 'http://www.ripedev.com/webservices/localtime.asmx?WSDL'
+
+# weather api
+$uri = 'http://www.webservicex.net/globalweather.asmx?WSDL'
+$webservice = New-WebServiceProxy -Uri $uri
+$x = [xml]$webservice.GetCitiesByCountry("Spain")
+$x.NewDataSet.Table
+
+
+
 
 
